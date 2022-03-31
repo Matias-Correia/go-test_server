453c5b204e9d4d4ecb56d8836c0fbbcce2b21974
Matias-Correia <matiasfrazaocorreia@gmail.com>
2022-03-31 14:36:11 +0100

    recording logs again in the db

--- server/logServer/main.go
+++ server/logServer/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
-	"fmt"
 
-	"google.golang.org/grpc"
 	pb "github.com/Matias-Correia/go-test_server/server/protologs"
+	"google.golang.org/grpc"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 
 	"database/sql"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -33,74 +34,71 @@ func (s *server) SendLogs(ctx context.Context, in *pb.Log) (*pb.Empty, error) {
 	log.Printf("ReceivedAt: %v", in.GetReceivedAt())
 	log.Printf("BlockRequestedAt: %v", in.GetBlockRequestedAt())
 	log.Printf("Duplicate: %v", in.GetDuplicate())
-	 
-	//recordReceivedLogs(s.database, in.GetBlockID(), in.GetLocalpeer(), in.GetRemotepeer(), in.GetSentAt(), in.GetReceivedAt(), in.GetBlockRequestedAt(), in.GetDuplicate())
+
+	recordReceivedLogs(s.database, in.GetBlockID(), in.GetLocalpeer(), in.GetRemotepeer(), in.GetSentAt(), in.GetReceivedAt(), in.GetBlockRequestedAt(), in.GetDuplicate())
 
 	return &(pb.Empty{}), nil
 }
 
 // Save the received logs in the BD
-func recordReceivedLogs( dbCon sql.DB, blockId string, localpeer string, remotepeer string, sentAt *timestamppb.Timestamp, receivedAt *timestamppb.Timestamp, blockRequestedAt *timestamppb.Timestamp, duplicate bool){
+func recordReceivedLogs(dbCon sql.DB, blockId string, localpeer string, remotepeer string, sentAt *timestamppb.Timestamp, receivedAt *timestamppb.Timestamp, blockRequestedAt *timestamppb.Timestamp, duplicate bool) {
 
-	//convert timestamps to DB format with milliseconds 
+	//convert timestamps to DB format with milliseconds
 	sentAtAsTime := fmt.Sprintf("%v", sentAt.AsTime().Format("2006-01-02 15:04:05.000"))
 	receivedAtAsTime := fmt.Sprintf("%v", receivedAt.AsTime().Format("2006-01-02 15:04:05.000"))
 	blockRequestedAtAsTime := fmt.Sprintf("%v", blockRequestedAt.AsTime().Format("2006-01-02 15:04:05.000"))
 
+	query := fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,SentAt,ReceivedAt,BlockRequestedAt,Duplicate) VALUES('%s','%s','%s','%v','%v','%v',%t)", blockId, localpeer, remotepeer, sentAtAsTime, receivedAt, blockRequestedAt, duplicate)
 
-	query := fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,SentAt,ReceivedAt,BlockRequestedAt,Duplicate) VALUES('%s','%s','%s','%v','%v','%v',%t)",blockId,localpeer,remotepeer,sentAtAsTime,receivedAt,blockRequestedAt,duplicate)
-
-	if sentAt == nil{
-		if receivedAt == nil{
-			if blockRequestedAt == nil{
-				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,Duplicate) VALUES('%s','%s','%s',%t)",blockId,localpeer,remotepeer,duplicate)
-			}else{
-				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,BlockRequestedAt,Duplicate) VALUES('%s','%s','%s','%v',%t)",blockId,localpeer,remotepeer,blockRequestedAtAsTime,duplicate)
+	if sentAt == nil {
+		if receivedAt == nil {
+			if blockRequestedAt == nil {
+				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,Duplicate) VALUES('%s','%s','%s',%t)", blockId, localpeer, remotepeer, duplicate)
+			} else {
+				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,BlockRequestedAt,Duplicate) VALUES('%s','%s','%s','%v',%t)", blockId, localpeer, remotepeer, blockRequestedAtAsTime, duplicate)
 			}
-		}else{
-			if blockRequestedAt == nil{
-				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,ReceivedAt,Duplicate) VALUES('%s','%s','%s','%v',%t)",blockId,localpeer,remotepeer,receivedAtAsTime,duplicate)
-			}else{
-				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,ReceivedAt,BlockRequestedAt,Duplicate) VALUES('%s','%s','%s','%v','%v',%t)",blockId,localpeer,remotepeer,receivedAtAsTime,blockRequestedAtAsTime,duplicate)
+		} else {
+			if blockRequestedAt == nil {
+				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,ReceivedAt,Duplicate) VALUES('%s','%s','%s','%v',%t)", blockId, localpeer, remotepeer, receivedAtAsTime, duplicate)
+			} else {
+				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,ReceivedAt,BlockRequestedAt,Duplicate) VALUES('%s','%s','%s','%v','%v',%t)", blockId, localpeer, remotepeer, receivedAtAsTime, blockRequestedAtAsTime, duplicate)
 			}
 		}
-	}else{
-		if receivedAt == nil{
-			if blockRequestedAt == nil{
-				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,SentAt,Duplicate) VALUES('%s','%s','%s','%v',%t)",blockId,localpeer,remotepeer,sentAtAsTime,duplicate)
-			}else{
-				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,SentAt,BlockRequestedAt,Duplicate) VALUES('%s','%s','%s','%v','%v',%t)",blockId,localpeer,remotepeer,sentAtAsTime,blockRequestedAtAsTime,duplicate)
+	} else {
+		if receivedAt == nil {
+			if blockRequestedAt == nil {
+				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,SentAt,Duplicate) VALUES('%s','%s','%s','%v',%t)", blockId, localpeer, remotepeer, sentAtAsTime, duplicate)
+			} else {
+				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,SentAt,BlockRequestedAt,Duplicate) VALUES('%s','%s','%s','%v','%v',%t)", blockId, localpeer, remotepeer, sentAtAsTime, blockRequestedAtAsTime, duplicate)
 			}
-		}else{
-			if blockRequestedAt == nil{
-				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,SentAt,ReceivedAt,Duplicate) VALUES('%s','%s','%s','%v','%v',%t)",blockId,localpeer,remotepeer,sentAtAsTime,receivedAtAsTime,duplicate)
-			}else{
-				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,SentAt,ReceivedAt,BlockRequestedAt,Duplicate) VALUES('%s','%s','%s','%v','%v','%v',%t)",blockId,localpeer,remotepeer,sentAtAsTime,receivedAtAsTime,blockRequestedAtAsTime,duplicate)
+		} else {
+			if blockRequestedAt == nil {
+				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,SentAt,ReceivedAt,Duplicate) VALUES('%s','%s','%s','%v','%v',%t)", blockId, localpeer, remotepeer, sentAtAsTime, receivedAtAsTime, duplicate)
+			} else {
+				query = fmt.Sprintf("INSERT INTO test_logs (BlockID,LocalPeer,RemotePeer,SentAt,ReceivedAt,BlockRequestedAt,Duplicate) VALUES('%s','%s','%s','%v','%v','%v',%t)", blockId, localpeer, remotepeer, sentAtAsTime, receivedAtAsTime, blockRequestedAtAsTime, duplicate)
 			}
 		}
 	}
-	
-	
+
 	fmt.Printf(query)
 	insert, err := dbCon.Query(query)
 
-    // if there is an error inserting, handle it
-    if err != nil {
-        panic(err.Error())
-    }
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
 
-    defer insert.Close()
+	defer insert.Close()
 
 }
 
-func openDataBaseConnection() sql.DB{
+func openDataBaseConnection() sql.DB {
 	db, err := sql.Open("mysql", "admin:raiz@(db:3306)/logs")
 	if err != nil {
-  		panic(err)
+		panic(err)
 	}
 	return *db
 }
-
 
 func main() {
 	lis, err := net.Listen("tcp", port)
